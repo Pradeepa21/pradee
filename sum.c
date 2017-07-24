c30a0d6ba816f3873f8af148e3f7868e7862d19a
Pradeepa21 <30360927+Pradeepa21@users.noreply.github.com>
2017-07-24 18:43:46 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+int i,sum=0,n;
+printf("enter the no of values");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("%d",&sum);
+}
