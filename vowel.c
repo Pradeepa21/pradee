fdfc7b008ee6b3dcc1eea410b586d005fd863f2d
Pradeepa21 <30360927+Pradeepa21@users.noreply.github.com>
2017-07-25 21:54:06 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+char ch;
+clrscr();
+printf("Enter the alphabet");
+scanf("%c",&ch);
+if(ch==a||ch==e||ch==i||ch==o||ch==u||ch==A||ch==E||ch==I||ch==O||ch==U)
+printf("The alphabet is a vowel);
+else
+printf("The alphabet is a constant");
+getch();
+}
